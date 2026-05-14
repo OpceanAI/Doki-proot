fe09d734e1511e81d54720629841727cf3f8c1fd
aguitauwu <212803252+aguitauwu@users.noreply.github.com>
2026-05-14 10:03:10 -0600

    proot: security hardening flags, hidden replacement fixed

--- proot/src/extension/doki_hidden/doki_hidden.h
+++ proot/src/extension/doki_hidden/doki_hidden.h
@@ -2,7 +2,7 @@
 #define DOKI_HIDDEN_H
 
 #define DOKI_HIDDEN_MAX_FILES 32
-#define DOKI_HIDDEN_REPLACEMENT "/doki_internal/hidden"
+#define DOKI_HIDDEN_REPLACEMENT "/dev/null"
 
 void doki_hidden_load_from_json(const char *json_msg);
 const char *doki_hidden_check_path(const char *path);
